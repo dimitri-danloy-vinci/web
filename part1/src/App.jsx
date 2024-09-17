cd43666aa161ae08520d1105963b3bda9bf93871
dimitri-danloy-vinci <129385593+dimitri-danloy-vinci@users.noreply.github.com>
2024-09-17 15:16:27 +0200

    1.1-1.2 : basic components

---
+++ part1/src/App.jsx
@@ -0,0 +1,17 @@
+import Content from './Content'
+
+const App = () => {
+  const course = 'Half Stack application development'
+
+  const totalExercises = 10 + 7 + 14
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <Content />
+      <p>Number of exercises {totalExercises}</p>
+    </div>
+  )
+}
+
+export default App
---
+++ part1/src/Content.jsx
@@ -0,0 +1,19 @@
+import Part from './Part'
+
+const Content = () => {
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ]
+
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  )
+}
+
+export default Content
